fa41ba6dfa2a068380d5f0ee20e817bcb2e64f77
microcai <microcaicai@gmail.com>
2010-02-21 12:59:21 +0800

    COPYRIGHT NOTICE FOR EVERYFILE

--- src/SourceView.c
+++ src/SourceView.c
@@ -1,5 +1,25 @@
 /*
- * SourceView.c
+ *  Linux Do - A new IDE for newbee
+ *
+ * Copyright (c) 2010-2010 microcai  <microcai@fedoraproject.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * SourceView.c - the master editor
  *
  *  Created on: 2010-2-20
  *      Author: cai
